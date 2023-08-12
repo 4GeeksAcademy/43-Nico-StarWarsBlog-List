74783fffdd5f18252b8f0edfddcf258797955ffc
Alexander-sys18 <119635571+Alexander-sys18@users.noreply.github.com>
2023-08-12 02:03:54 +0000

    resuelto errores en consola

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,3 +1,69 @@
+// const getState = ({ getStore, getActions, setStore }) => {
+// 	return {
+// 	  store: {
+// 		favorites: [],
+// 	  },
+// 	  actions: {
+// 		getDataPeople: async () => {
+// 		  try {
+// 			const store = getStore();
+// 			const result = await fetch("https://www.swapi.tech/api/people?page=1&limit=20");
+// 			const data = await result.json();
+			
+// 			setStore({ ...store, people: data.results });
+// 			console.log("API respondió bien con primera lista de url", data);
+// 			const actions = getActions();
+// 			await actions.mappingFetch();
+// 		  } catch (error) {
+// 			console.log("No se pudo recuperar lista de urls ", error);
+// 		  }
+// 		},
+// 		getDataPlanets: async () => {
+// 		  try {
+// 			const store = getStore();
+// 			const result = await fetch("https://www.swapi.tech/api/planets?page=1&limit=20");
+// 			const data = await result.json();
+			
+// 			setStore({ ...store, planets: data.results });
+// 			console.log("API respondió bien con primera lista de url", data);
+// 			const { mappingFetchPlanets } = getActions();
+// 			await mappingFetchPlanets();
+// 		  } catch (error) {
+// 			console.log("No se pudo recuperar lista de urls ", error);
+// 		  }
+// 		},
+  
+// 		setFavoritesCharacters: (characterName) => {
+// 			const store = getStore();
+// 			const favoriteCharacterAlreadyExist = store.favorites.includes(characterName);
+// 			if (!favoriteCharacterAlreadyExist) {
+// 			  setStore({ favorites: [...store.favorites, characterName] });
+// 			}
+// 		  },
+
+// 		  setFavorites: (newFavorites) => {
+// 			setStore({ favorites: newFavorites });
+// 		},
+  
+// 		setFavoritesPlanets: (planet) => {
+// 		  const store = getStore();
+// 		  const favoritePlanetAlreadyExist = store.favorites.includes(planet.result.properties.name);
+// 		  if (!favoritePlanetAlreadyExist) {
+// 			setStore({ favorites: [...store.favorites, planet.result.properties.name] });
+// 		  }
+// 		},
+  
+// 		deleteFavorite: (index) => {
+// 		  const store = getStore();
+// 		  const updatedFavorites = [...store.favorites];
+// 		  updatedFavorites.splice(index, 1);
+// 		  setStore({ favorites: updatedFavorites });
+// 		},
+// 	  },
+// 	};
+//   };
+  
+//   export default getState;
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 	  store: {
@@ -9,11 +75,9 @@ const getState = ({ getStore, getActions, setStore }) => {
 			const store = getStore();
 			const result = await fetch("https://www.swapi.tech/api/people?page=1&limit=20");
 			const data = await result.json();
-			
+  
 			setStore({ ...store, people: data.results });
 			console.log("API respondió bien con primera lista de url", data);
-			const actions = getActions();
-			await actions.mappingFetch();
 		  } catch (error) {
 			console.log("No se pudo recuperar lista de urls ", error);
 		  }
@@ -23,26 +87,24 @@ const getState = ({ getStore, getActions, setStore }) => {
 			const store = getStore();
 			const result = await fetch("https://www.swapi.tech/api/planets?page=1&limit=20");
 			const data = await result.json();
-			
+  
 			setStore({ ...store, planets: data.results });
 			console.log("API respondió bien con primera lista de url", data);
-			const { mappingFetchPlanets } = getActions();
-			await mappingFetchPlanets();
 		  } catch (error) {
 			console.log("No se pudo recuperar lista de urls ", error);
 		  }
 		},
   
 		setFavoritesCharacters: (characterName) => {
-			const store = getStore();
-			const favoriteCharacterAlreadyExist = store.favorites.includes(characterName);
-			if (!favoriteCharacterAlreadyExist) {
-			  setStore({ favorites: [...store.favorites, characterName] });
-			}
-		  },
-
-		  setFavorites: (newFavorites) => {
-			setStore({ favorites: newFavorites });
+		  const store = getStore();
+		  const favoriteCharacterAlreadyExist = store.favorites.includes(characterName);
+		  if (!favoriteCharacterAlreadyExist) {
+			setStore({ favorites: [...store.favorites, characterName] });
+		  }
+		},
+  
+		setFavorites: (newFavorites) => {
+		  setStore({ favorites: newFavorites });
 		},
   
 		setFavoritesPlanets: (planet) => {
@@ -64,3 +126,4 @@ const getState = ({ getStore, getActions, setStore }) => {
   };
   
   export default getState;
+  
